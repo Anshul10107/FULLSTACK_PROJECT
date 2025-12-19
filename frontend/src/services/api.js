7d8bb193b326793109e79ce9733f9552b1c99094
Anshul10107 <anshulsen10107@gmail.com>
2025-12-19 18:46:16 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://backend-service-yv9k.onrender.com/api"
 });
 
 export default API;
